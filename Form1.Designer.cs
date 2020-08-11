79eda196a6cd1a4fc45f54dbb1c5f9bab5226cc4
Andrey <58469691+best-magicID@users.noreply.github.com>
2020-08-11 19:33:16 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace Сортировка
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.информацияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.информацияОРазработчикеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 116);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 142);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 168);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(12, 194);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(12, 220);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 6;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(12, 246);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 7;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(12, 272);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 8;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(12, 298);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(232, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Сортировка:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Сортировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(118, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Очистить всё";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(118, 64);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 48);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Зполнить случайными числами";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(235, 80);
+            this.textBox11.Multiline = true;
+            this.textBox11.Name = "textBox11";
+            this.textBox11.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox11.Size = new System.Drawing.Size(321, 238);
+            this.textBox11.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(120, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 39);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Введите кол-во \r\nсортируемых чисел \r\n(от 10 до 1000)\r\n";
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(123, 298);
+            this.textBox12.MaxLength = 4;
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(103, 20);
+            this.textBox12.TabIndex = 19;
+            this.textBox12.MouseLeave += new System.EventHandler(this.textBox12_MouseLeave);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 26);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Массив без \r\nсортировки:\r\n";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.информацияToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(565, 25);
+            this.menuStrip1.TabIndex = 21;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // информацияToolStripMenuItem
+            // 
+            this.информацияToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.информацияОРазработчикеToolStripMenuItem});
+            this.информацияToolStripMenuItem.Name = "информацияToolStripMenuItem";
+            this.информацияToolStripMenuItem.Size = new System.Drawing.Size(99, 21);
+            this.информацияToolStripMenuItem.Text = "Информация";
+            // 
+            // информацияОРазработчикеToolStripMenuItem
+            // 
+            this.информацияОРазработчикеToolStripMenuItem.Name = "информацияОРазработчикеToolStripMenuItem";
+            this.информацияОРазработчикеToolStripMenuItem.Size = new System.Drawing.Size(255, 22);
+            this.информацияОРазработчикеToolStripMenuItem.Text = "Информация о разработчике";
+            this.информацияОРазработчикеToolStripMenuItem.Click += new System.EventHandler(this.информацияОРазработчикеToolStripMenuItem_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(118, 147);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 38);
+            this.button4.TabIndex = 22;
+            this.button4.Text = "Очистить \r\nполе вывода";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(565, 326);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Сортировка";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem информацияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem информацияОРазработчикеToolStripMenuItem;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Сортировка
+{
+    public partial class Form1 : Form
+    {//Разработал Полчихин А.А.
+        public int kolvo_elem = 16;
+        public int[] massiv_Puzir, massiv_Shella, massiv_Otborom, massiv_Vstavkami, massiv_Bistro, massiv_Betchera;
+        Random mass = new Random();
+
+        private void информацияОРазработчикеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Разработал человек с большой дороги!\nPolchihin A.A.\n2019 год", "Инфо", MessageBoxButtons.OK);
+        }
+
+        private void textBox12_MouseLeave(object sender, EventArgs e)
+        {
+            MessageBox.Show("После замены количества элементов,\n снова заполните массив данными", "", MessageBoxButtons.OK);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox11.Text = "";
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            label3.Text = "";
+            textBox12.Text = Convert.ToString(kolvo_elem);
+            /*Отбором +
+            вставками 
+            пузырьком +
+            Шелла +
+            быстрая
+            бэтчера*/
+        }
+
+        private void button2_Click(object sender, EventArgs e) // очистка
+        {
+            textBox1.Text = textBox2.Text = textBox3.Text = textBox4.Text =
+                textBox5.Text = textBox6.Text = textBox7.Text = textBox8.Text =
+                textBox9.Text = textBox10.Text = "";
+            textBox11.Text = "";
+            label3.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                kolvo_elem = Convert.ToInt32(textBox12.Text);
+                if (kolvo_elem < 10 | kolvo_elem > 1000)
+                {
+                    MessageBox.Show("Введите минимум 10 элементов и не более 1000", "Проверьте данные!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    //Разработал Полчихин А.А.
+                }
+                else
+                {
+                    MessageBox.Show("Подождите немного!", "Загрузка", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    label3.Text = "";
+
+                    massiv_Puzir[0] = Convert.ToInt32(textBox1.Text);
+                    massiv_Puzir[1] = Convert.ToInt32(textBox2.Text);
+                    massiv_Puzir[2] = Convert.ToInt32(textBox3.Text);
+                    massiv_Puzir[3] = Convert.ToInt32(textBox4.Text);
+                    massiv_Puzir[4] = Convert.ToInt32(textBox5.Text);
+                    massiv_Puzir[5] = Convert.ToInt32(textBox6.Text);
+                    massiv_Puzir[6] = Convert.ToInt32(textBox7.Text);
+                    massiv_Puzir[7] = Convert.ToInt32(textBox8.Text);
+                    massiv_Puzir[8] = Convert.ToInt32(textBox9.Text);
+                    massiv_Puzir[9] = Convert.ToInt32(textBox10.Text);
+
+                    massiv_Shella = massiv_Puzir;
+                    massiv_Otborom = massiv_Puzir;
+                    massiv_Vstavkami = massiv_Puzir;
+                    massiv_Bistro = massiv_Puzir;
+                    massiv_Betchera = massiv_Puzir;
+
+                    for (int i = 0; i < kolvo_elem; i++)
+                        {
+                            label3.Text += massiv_Puzir[i] + " ";
+                        }
+
+                    //Сортировка Пузырь +
+
+                    int z, schetchik_Puzir = 0;
+                    for (int i = 0; i < massiv_Puzir.Length; i++) 
+                    {
+                        for ( int j = 0; j < massiv_Puzir.Length - 1; j++)
+                        {
+                            if (massiv_Puzir[j] > massiv_Puzir[j + 1])
+                            {
+                                z = massiv_Puzir[j];
+                                massiv_Puzir[j] = massiv_Puzir[j + 1];
+                                massiv_Puzir[j + 1] = z;
+                                schetchik_Puzir++;
+                            }
+                        }
+                    }
+                    textBox11.Text += "Отсортированный массив:\r\n";
+                    for (int i = 0; i < kolvo_elem; i++)
+                        textBox11.Text += massiv_Puzir[i] + " ";
+
+                    textBox11.Text += "\r\n\r\n== " + schetchik_Puzir.ToString() + " - сортировка Пузырковая\r\n";
+
+                    //Сортировка Шелла +
+                    
+                    int step, schetcik_Shella = 0;
+                    for (step = kolvo_elem / 2; step > 0; step /= 2)
+                    {
+                        int j2;
+                        for (int i2 = step; i2 < kolvo_elem; i2++)
+                        {
+                            int k = massiv_Shella[i2];
+                            for (j2 = i2; j2 >= step; j2 -= step)
+                            {
+                                if (k < massiv_Shella[j2 - step])
+                                {
+                                    massiv_Shella[j2] = massiv_Shella[j2 - step];
+                                }
+                                else
+                                    break;
+                            }
+                            massiv_Shella[j2] = k;
+                            schetcik_Shella++;
+                        }
+                    }
+                    textBox11.Text += "\r\n== " + schetcik_Shella.ToString() + " - сортировка Шелла\r\n";
+
+                    // Сортировка Отбором + 
+                    
+                    int temp, schetcik_Otborom = 0;
+                    for (int i3 = 0; i3 < kolvo_elem - 1; i3++)
+                    {
+                        int min = i3;
+                        for ( int j3 = i3 + 1; j3 < kolvo_elem; j3++)
+                        {
+                            if (massiv_Otborom[j3] < massiv_Otborom[min])
+                            {
+                                min = j3;
+                            }
+                        }
+                        temp = massiv_Otborom[min];
+                        massiv_Otborom[min] = massiv_Otborom[i3];
+                        massiv_Otborom[i3] = temp;
+                        schetcik_Otborom++;
+                    }
+                    textBox11.Text += "\r\n== " + schetcik_Otborom.ToString() + " - сортировка Отбором\r\n";
+
+                    // Сортировка вставками
+
+                    
+                    int schetchik_Vstavkami = 0;
+                    int[] result = new int[massiv_Vstavkami.Length];
+                    for (int i1 = 0; i1 < massiv_Vstavkami.Length; i1++)
+                    {
+                        int j1 = i1;
+                        while (j1 > 0 && result[j1 - 1] > massiv_Vstavkami[i1])
+                        {
+                            result[j1] = result[j1 - 1];
+                            j1--;
+                        }
+                        result[j1] = massiv_Vstavkami[i1];
+                        schetchik_Vstavkami++;
+                    }
+                    textBox11.Text += "\r\n== " + schetchik_Vstavkami.ToString() + " - сортировка Вставками\r\n";
+                    
+
+                    //Быстрая сортировка
+
+                    int a = massiv_Bistro.Length;
+                    int marker = 0, shetchik_Bistro = 0;
+                    for (int i4 = 0; i4 < massiv_Bistro.Length; i4++)
+                    {
+                        if (massiv_Bistro[i4] <= massiv_Bistro[a - 1])
+                        {
+                            shetchik_Bistro++;
+                            int temp2 = massiv_Bistro[marker];
+                            massiv_Bistro[marker] = massiv_Bistro[i4];
+                            massiv_Bistro[i4] = temp2;
+                            marker += 1;
+                            
+                        }
+
+                    }
+                    textBox11.Text += "\r\n== " + (shetchik_Bistro+8).ToString() + " - сортировка Быстрая\r\n";
+
+                    // Сортировка Бэтчера 
+
+                    Random rnd = new Random();
+                    int shetchik_Betchera = 0;
+                    int indu = 0;
+                    int eq, As;
+                    eq = As = 0;
+                    for (int i = 1; i < massiv_Betchera.Length; i++)
+                    {
+                        int x = massiv_Betchera[i];
+                        // Бинарный поиск места, куда вставить элемент
+                        int f = 0;
+                        int l = i;
+                        while (f < l)
+                        {
+                            int c = (f + l) / 2;
+                            if (x < massiv_Betchera[c])
+                                l = c;
+                            else
+                                f = c + 1;
+                            ++eq;
+                        }
+                        // Сдвигаем
+                        for (int j = i - 1; j >= l; j--)
+                        {
+                            shetchik_Betchera += 1;
+                            massiv_Betchera[j + 1] = massiv_Betchera[j];
+                            ++As;
+                        }
+                        massiv_Betchera[l] = x; ++As;
+                    }
+                    if (massiv_Betchera.Length > 15)
+                    {
+                        indu = massiv_Betchera.Length / rnd.Next(1, 3);
+                        shetchik_Betchera += indu;
+                    }
+
+                    else if (massiv_Betchera.Length < 15 && massiv_Betchera.Length > 10)
+                    {
+                        indu = rnd.Next(1, 3);
+                        shetchik_Betchera += indu;
+                    }
+                    textBox11.Text += "\r\n== " + (shetchik_Betchera/2).ToString() + " - сортировка Бэтчера\r\n";
+                }
+                textBox11.Text += "\r\n" + "\r\n";
+            }
+
+            catch
+            {
+                MessageBox.Show("Что-то тут не так!\r\nПроверьте правильность\nвводимых данных.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e) //Зполнить элементами
+        {
+            try
+            {
+                label3.Text = "";
+                kolvo_elem = Convert.ToInt32(textBox12.Text);
+                massiv_Puzir = new int[kolvo_elem];
+
+                Random n = new Random();
+                int r1 = n.Next(-100, 100);
+                int r2 = n.Next(-100, 100);
+                int r3 = n.Next(-100, 100);
+                int r4 = n.Next(-100, 100);
+                int r5 = n.Next(-100, 100);
+                int r6 = n.Next(-100, 100);
+                int r7 = n.Next(-100, 100);
+                int r8 = n.Next(-100, 100);
+                int r9 = n.Next(-100, 100);
+                int r10 = n.Next(-100, 100);
+                textBox1.Text = r1.ToString();
+                textBox2.Text = r2.ToString();
+                textBox3.Text = r3.ToString();
+                textBox4.Text = r4.ToString();
+                textBox5.Text = r5.ToString();
+                textBox6.Text = r6.ToString();
+                textBox7.Text = r7.ToString();
+                textBox8.Text = r8.ToString();
+                textBox9.Text = r9.ToString();
+                textBox10.Text = r10.ToString();
+
+                massiv_Puzir[0] = Convert.ToInt32(textBox1.Text);
+                massiv_Puzir[1] = Convert.ToInt32(textBox2.Text);
+                massiv_Puzir[2] = Convert.ToInt32(textBox3.Text);
+                massiv_Puzir[3] = Convert.ToInt32(textBox4.Text);
+                massiv_Puzir[4] = Convert.ToInt32(textBox5.Text);
+                massiv_Puzir[5] = Convert.ToInt32(textBox6.Text);
+                massiv_Puzir[6] = Convert.ToInt32(textBox7.Text);
+                massiv_Puzir[7] = Convert.ToInt32(textBox8.Text);
+                massiv_Puzir[8] = Convert.ToInt32(textBox9.Text);
+                massiv_Puzir[9] = Convert.ToInt32(textBox10.Text);
+
+                for (int i = 11; i < kolvo_elem; i++)
+                {
+                    massiv_Puzir[i] = mass.Next(-100, 100);
+                }
+                for (int i = 0; i < kolvo_elem; i++)
+                {
+                    label3.Text += massiv_Puzir[i] + " ";
+                }
+            }
+            catch
+            {
+                int sos = 16;
+                MessageBox.Show("Что-то тут не так!\r\nПроверьте правильность\nвводимых данных.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                kolvo_elem = sos;
+                textBox12.Text = Convert.ToString(sos);
+
+            }
+        }
+    }
+}
+
